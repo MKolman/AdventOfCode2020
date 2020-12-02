be0feea4a5eb86f599c20ea1a01545169c5ceb76
Maks Kolman <makskolman@google.com>
2020-12-02 09:59:59 +0000

    Add short python solutions for fun

---
+++ golf/day01.py
@@ -0,0 +1,3 @@
+from itertools import combinations as c
+n=map(int,open("../input/day01.txt").read().split())
+print{x+y:x*y for x,y in c(n,2)}[2020],{x+y+z:x*y*z for x,y,z in c(n,3)}[2020]
---
+++ golf/day02.py
@@ -0,0 +1,7 @@
+o=t=0
+for l in open("../input/day02.txt"):
+ n,(c,_),p=l.split()
+ s,e=map(int,n.split('-'))
+ o+=s<=p.count(c)<=e
+ t+=(p[s-1] == c)^(p[e-1] == c)
+print o,t
