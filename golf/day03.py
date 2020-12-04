4690237888b57f402135c3c5dd0eb4ab632f5d24
Maks Kolman <makskolman@google.com>
2020-12-04 16:18:53 +0000

    Save 1 byte in day03

--- golf/day03.py
+++ golf/day03.py
@@ -1,2 +1,2 @@
-f=lambda n:sum(l[(n*i)%(len(l)-1)]=='#'if n else l[(i/2)%(len(l)-1)]=='#'and 0==i%2 for i,l in enumerate(open("../input/day03.txt")))
+f=lambda n:sum(l[(n*i)%(len(l)-1)]=='#'if n else l[(i/2)%(len(l)-1)]=='#'and 1-i%2 for i,l in enumerate(open("../input/day03.txt")))
 print f(3),f(1)*f(3)*f(5)*f(7)*f(0)
