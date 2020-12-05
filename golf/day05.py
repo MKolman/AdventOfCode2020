b652905f50202d84272646c7fff86e4970b4ca7d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-05 12:35:34 +0000

    Deploy MKolman/AdventOfCode2020 to github.com/MKolman/AdventOfCode2020.git:gh-pages

--- golf/day05.py
+++ golf/day05.py
@@ -1,2 +1,2 @@
 s=[sum(2**(9-i)*(l[i]in"BR")for i in range(10))for l in open("../input/day05.txt")]
-print max(s),[i-1 for i in n if i-2 in n and i-1 not in s]
+print max(s),[i-1 for i in s if i-2 in s and i-1 not in s]
