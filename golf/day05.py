67eca60a1f14458ff2209390ef31ad71f3cfcdfb
Maks Kolman <makskolman@google.com>
2020-12-05 22:41:20 +0000

    No brackets when bitshifting

--- golf/day05.py
+++ golf/day05.py
@@ -1,2 +1,2 @@
-s=[sum((l[i]in"BR")<<(9-i)for i in range(10))for l in open("../input/day05.txt")]
+s=[sum((l[i]in"BR")<<9-i for i in range(10))for l in open("../input/day05.txt")]
 print max(s),[i-1 for i in s if i-2 in s and i-1 not in s]
