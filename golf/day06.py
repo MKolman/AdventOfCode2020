a015bd4f85f2180aafc7769ecd1a288f25d24d03
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-06 20:32:36 +0000

    Deploy MKolman/AdventOfCode2020 to github.com/MKolman/AdventOfCode2020.git:gh-pages

--- golf/day06.py
+++ golf/day06.py
@@ -1,1 +1,1 @@
-print sum(len(reduce(set.union,g.split(),set()))+1j*len(reduce(set.intersection,g.split(),set(g)))for g in open("../input/day06.txt").read().split('\n\n'))
+print sum(len(set().union(*g.split()))+1j*len(set(g).intersection(*g.split()))for g in open("../input/day06.txt").read().split('\n\n'))
