bf9f510d55eef76ef031330fdd45880c514a1f29
Maks Kolman <makskolman@google.com>
2020-12-08 18:18:37 +0000

    Reduce day01 golf for 15bytes

--- golf/day01.py
+++ golf/day01.py
@@ -1,3 +1,3 @@
 from itertools import combinations as c
-n=map(int,open("../input/day01.txt").read().split())
+n=map(int,open("../input/day01.txt"))
 print{x+y:x*y for x,y in c(n,2)}[2020],{x+y+z:x*y*z for x,y,z in c(n,3)}[2020]
