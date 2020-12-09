ddbddacba5f3cd3b57830cc1275cb2baa0dfc076
Maks Kolman <makskolman@google.com>
2020-12-09 21:56:20 +0000

    Save bytes by aliasing range

--- golf/day09.py
+++ golf/day09.py
@@ -1,3 +1,4 @@
 n=map(int,open("../input/day09.txt"))
-m=[x for i,x in enumerate(n[25:])if x not in[b+n[j+i]for j in range(25)for b in n[i:j+i]]][0]
-print m,{sum(n[i:j]):min(n[i:j])+max(n[i:j])for j in range(len(n))for i in range(j-2)}[m]
+r=range
+m=[x for i,x in enumerate(n[25:])if x not in[b+n[j+i]for j in r(25)for b in n[i:j+i]]][0]
+print m,{sum(n[i:j]):min(n[i:j])+max(n[i:j])for j in r(len(n))for i in r(j-2)}[m]
