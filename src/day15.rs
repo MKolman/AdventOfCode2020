f1af774ac7f6a3918a35660b365021814962a3c1
Maks Kolman <makskolman@google.com>
2020-12-15 08:39:41 +0000

    Solve day 15

--- src/day15.rs
+++ src/day15.rs
@@ -1,17 +1,40 @@
+use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
 
+fn solve(input: &String, length: usize) -> String {
+	let mut last: HashMap<u64, usize> = HashMap::new();
+	let nums: Vec<u64> = input.split(",").map(|n| n.parse().unwrap()).collect();
+	for (i, n) in nums[..nums.len()-1].iter().enumerate() {
+		last.insert(*n, i);
+	}
+	let mut prev = nums[nums.len()-1];
+	for i in nums.len()-1..length-1 {
+		println!("{:?}", prev);
+		if last.contains_key(&prev) {
+			let next = i - *last.get(&prev).unwrap();
+			last.insert(prev, i);
+			prev = next as u64;
+		} else {
+			last.insert(prev, i);
+			prev = 0;
+		}
+	}
+	return prev.to_string();
+}
+
 #[wasm_bindgen(js_name = day15_part_one)]
-pub fn part_one(_: String) -> String {
-	return "Not implemented".to_string();
+pub fn part_one(input: String) -> String {
+	return solve(&input, 2020);
 }
 
 #[wasm_bindgen(js_name = day15_part_two)]
-pub fn part_two(_: String) -> String {
-	return "Not implemented".to_string();
+pub fn part_two(input: String) -> String {
+	return solve(&input, 30000000);
 }
 
 #[test]
 fn test() {
-	assert_eq!(part_one("".to_string()), "Not implemented".to_string());
-	assert_eq!(part_two("".to_string()), "Not implemented".to_string());
+	let input = crate::common::get_input(15);
+	assert_eq!(part_one(input.clone()), "492".to_string());
+	assert_eq!(part_two(input.clone()), "63644".to_string());
 }
